18bca1bfe8127e6eb7c15a3f26e3ca66a0091a51
Pablo99pc <paoloc1999@gmail.com>
2020-12-08 16:42:39 +0100

    secondo commit

--- src/negri.java
+++ src/negri.java
@@ -1,6 +1,5 @@
 public class negri {
     public static void main(String args[]){
-        System.out.println("ciao6");
-        System.out.println("ciao5");
+        System.out.println("diobello");
     }
 }
